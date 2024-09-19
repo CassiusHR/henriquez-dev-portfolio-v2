53befe0b1952aba690603dc882adc3c369702c1b
Carlos Henriquez Montenegro <LCHenriquez@riamoneytransfer.com>
2024-09-18 21:03:41 -0600

    added header threejs and motion dependencies

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <div className="absolute top-0 left-0 flex flex-row justify-between items-center w-full py-8 px-8 z-10 bg-white backdrop-blur-2xl bg-opacity-30">
+        <div className="absolute top-0 left-0 flex flex-row justify-between items-center w-full py-8 px-8 z-10 bg-white backdrop-blur-2xl bg-opacity-60 border-b-[1px] border-neutral-200">
             <Image src="/logo.svg" alt="logo" width={32} height={32} />
         </div>
 
